4f37f025535ec39b7ed94e346f3d2866083548e7
ahm0xc <codingwebsa@gmail.com>
2025-06-04 14:54:57 +0600

    add: integrate trpc and update dependencies in mobile app

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,7 +1,12 @@
 import { Stack } from "expo-router";
 
 import "~/styles/globals.css";
+import { TRPCProvider } from "~/trpc/provider";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <TRPCProvider>
+      <Stack />
+    </TRPCProvider>
+  );
 }
